b6494bcda4dbfeb606b2e18ec4f3c56420ec3ce7
eZWALT <walterthehshadow333@gmail.com>
2023-05-12 18:10:33 +0200

    totes les fotos ja excepte taules cutoff, ja podem començar a redactar

--- LAB/LABORATORI4/SourceCode/multisort-tareadorTREE.c
+++ LAB/LABORATORI4/SourceCode/multisort-tareadorTREE.c
@@ -19,33 +19,52 @@ void basicmerge(long n, T left[n], T right[n], T result[n*2], long start, long l
 
 void merge(long n, T left[n], T right[n], T result[n*2], long start, long length) {
     if (length < MIN_MERGE_SIZE*2L) {
-        tareador_start_task("leafmerge");
         basicmerge(n, left, right, result, start, length);
-	    tareador_end_task("leafmerge");
     } else {
         // Recursive decomposition
+	tareador_start_task("merge");
         merge(n, left, right, result, start, length/2);
+	tareador_end_task("merge");
+
+	tareador_start_task("merge");
         merge(n, left, right, result, start + length/2, length/2);
+	tareador_end_task("merge");
     }
 }
 
 void multisort(long n, T data[n], T tmp[n]) {
     if (n >= MIN_SORT_SIZE*4L) {
         // Recursive decomposition
+	tareador_start_task("multi");
         multisort(n/4L, &data[0], &tmp[0]);
+	tareador_end_task("multi");
+
+	tareador_start_task("multi");
         multisort(n/4L, &data[n/4L], &tmp[n/4L]);
+	tareador_end_task("multi");
+	
+	tareador_start_task("multi");
         multisort(n/4L, &data[n/2L], &tmp[n/2L]);
+	tareador_end_task("multi");
+
+	tareador_start_task("multi");
         multisort(n/4L, &data[3L*n/4L], &tmp[3L*n/4L]);
-
+	tareador_end_task("multi");
+	
+	tareador_start_task("merge");
         merge(n/4L, &data[0], &data[n/4L], &tmp[0], 0, n/2L);
-        merge(n/4L, &data[n/2L], &data[3L*n/4L], &tmp[n/2L], 0, n/2L);
+	tareador_end_task("merge");
 
+	tareador_start_task("merge");
+        merge(n/4L, &data[n/2L], &data[3L*n/4L], &tmp[n/2L], 0, n/2L);
+	tareador_end_task("merge");
+	
+	tareador_start_task("merge");
         merge(n/2L, &tmp[0], &tmp[n/2L], &data[0], 0, n);
+	tareador_end_task("merge");
     } else {
         // Base case
-	tareador_start_task("leafmulti");
         basicsort(n, data);
-	tareador_end_task("leafmulti");
     }
 }
 
